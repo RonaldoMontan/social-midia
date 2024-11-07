badba5161dcc2079411c1df5fe2de53516e61d0b
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-06 22:04:55 -0300

    function need requests not response

--- api/src/router/controllers/users.go
+++ api/src/router/controllers/users.go
@@ -2,27 +2,27 @@ package controllers
 
 import "net/http"
 
-func CreateUser(w http.ResponseWriter, r *http.Response){
+func CreateUser(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Create User !"))
 }
 
-func SearchAllUsers(w http.ResponseWriter, r *http.Response){
+func SearchAllUsers(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Searching for all users !"))
 }
 
-func SearchUser(w http.ResponseWriter, r *http.Response){
+func SearchUser(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Search one User !"))
 }
 
-func AlterUser(w http.ResponseWriter, r *http.Response){
+func AlterUser(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Alter information the User !"))
 }
 
-func DeleteUser(w http.ResponseWriter, r *http.Response){
+func DeleteUser(w http.ResponseWriter, r *http.Request){
 
 	w.Write([]byte("Delete User !"))
 }
