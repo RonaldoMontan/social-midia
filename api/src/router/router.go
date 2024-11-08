911b7fff4cc3f50e40c62db68ae11e91d5c3ed50
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-07 23:21:12 -0300

    Create function configute for return all handleFunc

--- api/src/router/router.go
+++ api/src/router/router.go
@@ -1,8 +1,14 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"api/src/router/routers"
+
+	"github.com/gorilla/mux"
+)
 
 func Generate() *mux.Router {
 
-	return mux.NewRouter()
+	r := mux.NewRouter()
+
+	return routers.Configure(r)
 }
--- api/src/router/routers/routers.go
+++ api/src/router/routers/routers.go
@@ -1,10 +1,26 @@
 package routers
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Route struct {
 	Uri string
 	Metodo string
 	Function func(http.ResponseWriter, *http.Request)
 	NeedAuth bool
+}
+
+// Configute coloca todas as rotas dentro do router
+func Configure(r *mux.Router) *mux.Router {
+
+	routers := routeUsers
+
+	for _, route := range routers {
+		r.HandleFunc(route.Uri, route.Function).Methods(route.Metodo)
+	}
+
+	return r
 }
