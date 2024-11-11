8c910bb2a6f9c7c5f6030d07c233183be40d7905
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-10 22:21:22 -0300

    Using variable of ambient for information

--- api/main.go
+++ api/main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 func main(){
 
+	config.LoadConfig()
+	fmt.Printf("API Runing in port: %d\n", config.Port)
+
 	r := router.Generate()
 
-	log.Fatal(http.ListenAndServe(":5000", r))
+	log.Fatal(http.ListenAndServe(
+		fmt.Sprintf(":%d", config.Port), r))
 }
