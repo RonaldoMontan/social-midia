250c477a8237fc2fc1294338fbca6ab3b269ca27
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-13 00:39:23 -0300

    Create method for validate and format, required field when create user

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 //abstração para representar um usuario navegando na rede social
 type User struct {
@@ -10,5 +14,32 @@ type User struct {
 	Email string `json:"email, omitempty"`
 	Password string `json:"password, omitempty"`
 	CreatedAt time.Time `json:"createdAt, omitempty"`
+}
 
+func (user *User) Validate() error {
+
+	if user.Name == "" {
+		return errors.New("The name is required")
+	}
+
+	if user.Nick == "" {
+		return errors.New("The Nick is required")
+	}
+
+	if user.Email == "" {
+		return errors.New("The email is required")
+	}
+
+	if user.Password == "" {
+		return errors.New("The password is required")
+	}
+
+	return nil
+}
+
+func (user *User) Format() {
+
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
