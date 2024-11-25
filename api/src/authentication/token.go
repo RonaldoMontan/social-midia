5778472f4b0623581bc5b7b5d0c7e17ec9961346
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-24 21:45:00 -0300

    create functions extract informations of token and func to check keys

--- api/src/authentication/token.go
+++ api/src/authentication/token.go
@@ -2,6 +2,10 @@ package authentication
 
 import (
 	"api/src/config"
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
@@ -20,4 +24,39 @@ func CreateToken(userId uint64) (string, error){
 
 	//Assinatura do token usando uma chava do arquivo .env
 	return token.SignedString([]byte(config.SecretKey))
+}
+
+//Verifica se o token passado na requisição é valido
+func ValidateToken(r  *http.Request) error {
+	tokenString := extractToken(r)
+	token, erro := jwt.Parse(tokenString, returnKeysCheck)
+
+	if erro != nil {
+		return erro
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		return nil
+	}
+
+	return errors.New("token invalid")
+
+}
+
+func extractToken(r *http.Request) string {
+
+	token := r.Header.Get("Authorization")
+
+	if len(strings.Split(token, " ")) == 2 {
+		return strings.Split(token, " ")[1]
+	}
+	return ""
+}
+
+func returnKeysCheck(token *jwt.Token) (interface{}, error) {
+
+	if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+		return nil, fmt.Errorf("method of signature unexpected ! %v", token.Header["alg"])
+	}
+	return config.SecretKey, nil
 }
