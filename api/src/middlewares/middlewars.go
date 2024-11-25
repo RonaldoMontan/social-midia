2d1ee5255253df00ce1520909debe4ce5f07757f
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-24 21:46:05 -0300

    implemented functions

--- api/src/middlewares/middlewars.go
+++ api/src/middlewares/middlewars.go
@@ -1,26 +1,31 @@
 package middlewares
 
 import (
-	"fmt"
+	"api/src/authentication"
+	"api/src/response"
 	"log"
 	"net/http"
 )
 
 //Mostra infromações das rotas no terminal
-func Logger(next http.HandlerFunc) http.HandlerFunc{
+func Logger(nextFunc http.HandlerFunc) http.HandlerFunc{
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("\n %s %s %s", r.Method, r.RequestURI, r.Host)
-		next(w, r)
+		nextFunc(w, r)
 	}
 }
 
 
 //Verifica se o usuario está autenticado para fazer requisições.
-func Authenticate(next http.HandlerFunc) http.HandlerFunc {
+func Authenticate(nextFunc http.HandlerFunc) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request){
-		fmt.Println("autenticando...")
-		next(w, r)
+		
+		if erro := authentication.ValidateToken(r); erro != nil {
+			response.Erro(w, http.StatusUnauthorized, erro)
+			return
+		}
+		nextFunc(w, r)
 	}
 }
