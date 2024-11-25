fea796ca6292329813ca414b085e57d97ee2a2fb
RonaldoMontan <ronaldogithub@gmail.com>
2024-11-24 21:47:33 -0300

    quick commands for db

---
+++ api/sql/sql.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS devbook;
+Use devbook;
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id int auto_increment primary key,
+    name varchar(50) not null,
+    nick varchar(50) not null unique,
+    email varchar(50) not null unique,
+    password varchar(100) not null,
+    createdAt timestamp default current_timestamp()
+) ENGINE=INNODB;
+
