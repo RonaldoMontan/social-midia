8ca5c4a70eb93849c056aec8f159519f75461771
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-09 23:05:14 -0300

    sql comands to create table of publications

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE IF NOT EXISTS devbook;
+
 Use devbook;
+
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS followers;
+DROP TABLE IF EXISTS publication;
+
 CREATE TABLE users(
     id int auto_increment primary key,
     name varchar(50) not null,
@@ -21,4 +25,16 @@ CREATE TABLE followers (
     REFERENCES users(id) 
     ON DELETE CASCADE,
     PRIMARY KEY (user_id, follower_id)
+) ENGINE=INNODB;
+
+CREATE TABLE publication (
+    publication_id int auto_increment primary key,
+    title varchar(50) NOT NULL,
+    content varchar(320) NOT NULL,
+    author_id int NOT NULL,
+    FOREIGN KEY (author_id)
+    REFERENCES users(id)
+    ON DELETE CASCADE,    
+    likes int DEFAULT 0,
+    createdAt timestamp default current_timestamp()
 ) ENGINE=INNODB;
