b10c4ea2dacc55ab3cfdc43daa5c29d6961c9823
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-10 23:17:00 -0300

    Declaration of the function

---
+++ api/src/router/controllers/publication.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "net/http"
+
+
+
+func CreatePublication(w http.ResponseWriter, r *http.Request){	
+
+}
+
+// Traz as publicações que apareceriam no feed do usuario
+func SearchPublications(w http.ResponseWriter, r *http.Request){
+
+}
+
+// Traz uma unica publicação
+func SearchPublication(w http.ResponseWriter, r *http.Request){
+
+}
+
+func UpdatePublication(w http.ResponseWriter, r *http.Request){
+
+}
+
+func DeletePublication(w http.ResponseWriter, r *http.Request){
+
+}
