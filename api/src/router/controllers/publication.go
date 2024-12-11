2e66e585e9056d234b56b787f7a1c915ce402b1d
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-11 00:10:56 -0300

    Developing function CreatePublication

--- api/src/router/controllers/publication.go
+++ api/src/router/controllers/publication.go
@@ -1,11 +1,48 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/authentication"
+	"api/src/db"
+	"api/src/models"
+	"api/src/repositori"
+	"api/src/response"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 
 
 func CreatePublication(w http.ResponseWriter, r *http.Request){	
 
+	userIdOnToken, erro := authentication.ExtractUserId(r)
+	if erro != nil {
+		response.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	bodyRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		response.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publication models.Publication
+	if erro = json.Unmarshal(bodyRequest, &publication); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	publication.AuthorId = userIdOnToken
+
+	db, erro := db.Connect()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+	}
+	defer db.Close()
+
+	repositori := repositori.NewRepositoriPublication(db)
+	publication.Id, erro := repositori.CreatePublication(publication)
 }
 
 // Traz as publicações que apareceriam no feed do usuario
