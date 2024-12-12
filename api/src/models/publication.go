f2d1dbc4dce4b57ce308629853da0fcbac447168
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-11 23:18:38 -0300

    implemented validation and format to publication

--- api/src/models/publication.go
+++ api/src/models/publication.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"errors"
+	"strings"
+	"time"
+)
 
 type Publication struct {
 	Id uint64 	`json:"id, omitempty"`
@@ -10,4 +14,33 @@ type Publication struct {
 	AuthorNick string `json:"AuthorNick, omitempty"`
 	Likes uint64 `json:"likes"`
 	CreatedAt time.Time `json:"createdAt, omitempty"`
+}
+
+// Chama os métodos para validar e formatar a publicação
+func (publication *Publication) Prepare() error {
+
+	if erro := publication.validate(); erro != nil {
+		return erro
+	}
+
+	publication.format()
+	return nil
+}
+
+func (publication *Publication) validate() error {
+
+	if publication.Title == "" {
+		return errors.New("The publication must have a title")
+	}
+
+	if publication.Content == "" {
+		return errors.New("The publication must have a content")
+	}
+	return nil
+}
+
+func (publication *Publication) format() {
+
+	publication.Title = strings.TrimSpace(publication.Title)
+	publication.Content = strings.TrimSpace(publication.Content)	
 }
