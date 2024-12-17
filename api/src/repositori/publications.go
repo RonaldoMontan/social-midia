4c74c1cf73b33dba794ed5277af72d4b59b4b675
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-17 17:45:03 -0300

    Create Select to search for post from the same user and their followers

--- api/src/repositori/publications.go
+++ api/src/repositori/publications.go
@@ -38,6 +38,51 @@ func (repositori Publications) CreatePublication(Publications models.Publication
 	return uint64(lastId), nil
 }
 
+//Traz as publicações dos seguidores que o usuario segue e também traz as suas publicações.
+func (repositori Publications) SearchAllPublications(userIdOnToken uint64) ([]models.Publication, error){
+	
+	row, erro := repositori.db.Query(`
+		SELECT DISTINCT 
+			p.*,
+			u.nick
+		FROM publication p
+		INNER JOIN users u on u.id = p.author_id
+		INNER JOIN followers f on f.user_id  = p.author_id 
+		WHERE 
+			u.id = ? or f.follower_id = ?
+		ORDER BY 
+			1 DESC `, 
+		
+		userIdOnToken, userIdOnToken, )
+
+	if erro != nil {
+		return nil, erro
+	}
+	defer row.Close()
+
+	var publications []models.Publication
+
+	for row.Next() {
+
+		var publication models.Publication
+
+		if erro = row.Scan(
+			&publication.Id,
+			&publication.Title,
+			&publication.Content,
+			&publication.AuthorId,
+			&publication.Likes,
+			&publication.CreatedAt,
+			&publication.AuthorNick,
+
+		); erro != nil {
+			return nil, erro
+		}
+		publications = append(publications, publication)
+	}
+	return publications, nil
+}
+
 func (repositori Publications) SearchPublicationId(publicationId uint64) (models.Publication, error){
 	
 	row, erro := repositori.db.Query(`
