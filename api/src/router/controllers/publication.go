130373ada597262a2cee815cb7bef4961a446d4c
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-17 23:08:31 -0300

    Read user from token, verify author id with user id token, to update informations

--- api/src/router/controllers/publication.go
+++ api/src/router/controllers/publication.go
@@ -7,6 +7,8 @@ import (
 	"api/src/repositori"
 	"api/src/response"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"io"
 	"net/http"
 	"strconv"
@@ -116,6 +118,64 @@ func SearchPublication(w http.ResponseWriter, r *http.Request){
 }
 
 func UpdatePublication(w http.ResponseWriter, r *http.Request){
+
+	userIdOnToken, erro := authentication.ExtractUserId(r)
+	if erro != nil {
+		response.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	parameters := mux.Vars(r)
+	fmt.Println(parameters)
+	publicationId, erro := strconv.ParseUint(parameters["publicationsId"], 10, 64)
+	if erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := db.Connect()
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+	defer db.Close()
+
+	repositori :=repositori.NewRepositoriPublication(db)
+	publicationSaveOnDataBase, erro := repositori.SearchPublicationId(publicationId)
+	if erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if publicationSaveOnDataBase.AuthorId != userIdOnToken {
+
+		response.Erro(w, http.StatusForbidden, errors.New("you do not have permission to change this"))
+		return
+	}
+
+	bodyRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		response.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+
+	var publication models.Publication
+	if erro = json.Unmarshal(bodyRequest, &publication); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = publication.Prepare(); erro != nil {
+		response.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	if erro = repositori.AlterPublication(publicationId, publication); erro != nil {
+		response.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	response.JSON(w, http.StatusOK, nil)
 
 }
 
