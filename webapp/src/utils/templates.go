84862d71291f2dc6e5e6a826989d364a15901494
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-23 00:41:51 -0300

    Find the files.html to render

---
+++ webapp/src/utils/templates.go
@@ -0,0 +1,20 @@
+package utils
+
+import (
+	"html/template"
+	"net/http"
+)
+
+var templates *template.Template
+
+// Localiza e insere os templates html na variavel 'template'
+func LoadingTemplates() {
+
+	templates = template.Must(template.ParseGlob("views/*.html"))
+}
+
+// Renderiza uma página html na tela
+func ExecTemplate(w http.ResponseWriter, template string, data interface{}) {
+
+	templates.ExecuteTemplate(w, template, data)	
+}
