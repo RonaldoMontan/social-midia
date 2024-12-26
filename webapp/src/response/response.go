3a44a0ef358b77db51e04b42e23e744b8deb23c3
RonaldoMontan <ronaldogithub@gmail.com>
2024-12-25 22:50:28 -0300

    handles status code and reponse default

---
+++ webapp/src/response/response.go
@@ -0,0 +1,31 @@
+package response
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+// Representa a resposta de erro na API
+type ErroAPI struct {
+	Erro string `json:"erro"`
+}
+
+//Retorna uma resposta em formato json para requisições
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	
+	w.Header().Set("Content-Type", "aplication/json")
+	w.WriteHeader(statusCode)
+
+	if erro := json.NewEncoder(w).Encode(data); erro != nil {
+		log.Fatal(erro)
+	}
+}
+
+// Trata as requisições com status_code 400 ou superior
+func HandleStatusCode(w http.ResponseWriter, r *http.Response) {
+
+	var erro ErroAPI
+	json.NewDecoder(r.Body).Decode(&erro)
+	JSON(w, r.StatusCode, erro)
+}
