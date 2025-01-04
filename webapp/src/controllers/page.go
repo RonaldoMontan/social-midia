61ba945117a80a2c54d4678d13657c262109c60a
RonaldoMontan <ronaldogithub@gmail.com>
2025-01-04 15:24:52 -0300

    get information to render in home.html

--- webapp/src/controllers/page.go
+++ webapp/src/controllers/page.go
@@ -1,11 +1,14 @@
 package controllers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"webapp/src/config"
+	"webapp/src/models"
 	"webapp/src/requisitions"
-	
+	"webapp/src/response"
+
 	"webapp/src/utils"
 )
 
@@ -24,8 +27,24 @@ func LoadUserRegstrationPage(w http.ResponseWriter, r *http.Request) {
 func LoadingMainPage(w http.ResponseWriter, r *http.Request) {
 
 	url := fmt.Sprintf("%s/search-publications", config.APIURL)
-	response, erro := requisitions.MakeRequisitionWithAuth(r, http.MethodGet, url, nil)
+	
+	publicationResponse, erro := requisitions.MakeRequisitionWithAuth(r, http.MethodGet, url, nil)
+	if erro != nil {
+		response.JSON(w, http.StatusInternalServerError, response.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer publicationResponse.Body.Close()
 
-	fmt.Println(response, erro)
-	utils.ExecTemplate(w, "home.html", nil)
+	if publicationResponse.StatusCode >= 400 {
+		response.HandleStatusCode(w, publicationResponse)
+		return
+	}
+
+	var publications []models.Publication
+	if erro = json.NewDecoder(publicationResponse.Body).Decode(&publications); erro != nil {
+		response.JSON(w, http.StatusUnprocessableEntity, response.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	utils.ExecTemplate(w, "home.html", publications)
 }
