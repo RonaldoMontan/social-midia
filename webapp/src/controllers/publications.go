b720886e94c8d552de5e4cd9925be2286a8c9867
RonaldoMontan <ronaldogithub@gmail.com>
2025-01-18 16:03:36 -0300

    Create function likePublication that read values of url and build url to send API

--- webapp/src/controllers/publications.go
+++ webapp/src/controllers/publications.go
@@ -5,9 +5,12 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
 	"webapp/src/requisitions"
 	"webapp/src/response"
+
+	"github.com/gorilla/mux"
 )
 
 //Lê as informações do front (formulario) e chama a API para cadastrar uma publicação no banco de dados
@@ -40,4 +43,31 @@ func CreatePublication(w http.ResponseWriter, r*http.Request) {
 	}
 
 	response.JSON(w, responsePublication.StatusCode, nil)
+}
+
+// Chama a API para curtir a publicação
+func LikePublication(w http.ResponseWriter, r *http.Request) {
+
+	paramters := mux.Vars(r)
+	publicationId, erro := strconv.ParseUint(paramters["publicationId"], 10, 64)
+	if erro != nil {
+		response.JSON(w, http.StatusBadRequest, response.ErroAPI{Erro: erro.Error()})
+		return
+	}
+
+	url := fmt.Sprintf("%s/publication/%d/like", config.APIURL, publicationId)
+	
+	responseLike, erro := requisitions.MakeRequisitionWithAuth(r, http.MethodPost, url, nil)
+	if erro != nil {
+		response.JSON(w, http.StatusInternalServerError, response.ErroAPI{Erro: erro.Error()})
+		return
+	}
+	defer responseLike.Body.Close()
+
+	if responseLike.StatusCode >= 400 {
+		response.HandleStatusCode(w, responseLike)
+		return
+	}
+
+	response.JSON(w, responseLike.StatusCode, nil)
 }
