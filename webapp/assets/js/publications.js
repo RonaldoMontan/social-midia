e2dc7610146fb5fe787c7af15f54a4acd3c4f48d
RonaldoMontan <ronaldogithub@gmail.com>
2025-01-18 15:59:02 -0300

    Create function that make publication, find and read value of publicationId

--- webapp/assets/js/publications.js
+++ webapp/assets/js/publications.js
@@ -1,4 +1,5 @@
-$('#new-publication').on('submit', createPublication);
+$('#new-publication').on('submit', createPublication); //# é para referenciar id
+$('.like-publication').on('click', likePublication);// . é para referenciar uma classe
 
 function createPublication(event) {
     
@@ -17,4 +18,29 @@ function createPublication(event) {
         console.log(erro);
         alert("Error");
     });
+}
+
+function likePublication(event) {
+    console.log("curtiu a publicação ai ?")
+
+    const elementClick = $(event.target);
+    const publicationId = elementClick.closest('div').data('publication-id');//pega a classe a cima do evento clicado
+
+    elementClick.prop('disabled', true);//desabilita o elemento até finalizar a requisição
+
+    $.ajax({
+        url: `/publication/${publicationId}/like`,
+        method: "POST"
+    }).done(function() {
+        const contLike = elementClick.next('span');
+        const quantityLike = parseInt(contLike.text());
+        
+        contLike.text(quantityLike +1 );
+
+    }).fail(function() {
+        alert("não deu certo");
+    
+    }).always(function() {
+        elementClick.prop('disabled', false);// habilita o botão novamente após concluir a requisição
+    })
 }
